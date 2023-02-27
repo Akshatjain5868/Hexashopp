e468e363db9a96abc32595dfef4ee2fac222df5a
Nancy Goswami <018tmpind@Nancys-MacBook-Pro.local>
2023-02-27 17:46:14 +0530

    allignments improved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    # private
+    private
 	
   	def current_cart
 		Cart.find(session[:cart_id])
@@ -8,12 +8,6 @@ class ApplicationController < ActionController::Base
 		cart = Cart.create
 		session[:cart_id] = cart.id
 		cart
-	end
-
-	def cart_icon_current_user
-		if current_user
-		  @cart_icon_current_user = Cart.find(session[:cart_id])
-		end
 	end
 
 end
